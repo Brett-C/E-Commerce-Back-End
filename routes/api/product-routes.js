a040195a68d17ce08fcd87162ea8da6ad5125ca1
Brett C <brcannon@bellsouth.net>
2023-11-15 16:56:15 -0600

    updated the product routes to be able to find all, find one product, as well as create a product and delete a product by its id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,19 +4,43 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [Category, Tag]
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [Category, Tag]
+    
+    })
+    if (!productData) {
+      res.status(400).json({ message: 'Product does not match this id!'});
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -92,8 +116,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const produtctData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!produtctData) {
+      res.status(400).json({ message: 'Product does not match this id!'});
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
